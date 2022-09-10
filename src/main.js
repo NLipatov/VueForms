ab8e9c1ef04836b4cadd60699605c087406bac84
Nikita Lipatov <6stringsohei@gmail.com>
2022-09-10 17:52:44 +0300

    FontAwesome icons added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import "bootstrap/dist/css/bootstrap.css"
+//Bootstrap imports:
+import "bootstrap/dist/css/bootstrap.css";
+
+//FontAwesome imports:
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 
-const app = createApp(App);
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon);
+
+library.add(faPlane)
 
 app.use(createPinia());
 

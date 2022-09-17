694f97c2be7a7ac7fb803e84e6c624f6a3a29fce
Nikita Lipatov <6stringsohei@gmail.com>
2022-09-18 01:31:03 +0300

    table layout

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -3,17 +3,40 @@
 </script>
 
 <template>
-    <div class="up-divider">
-
+    <div class="view-container">
+        <div class="up-divider">
+        </div>
+        <div class="table-container">
+            <div class="table-content">
+                <hot-table :data="data" :rowHeaders="true" :colHeaders="true"></hot-table>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
+    .view-container {
+        position: absolute;
+        top: 0;
+        z-index: -1;
+    }
+    .table-container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     .up-divider {
         position: absolute;
         height: 50vh;
         width: 100vw;
         background-color: #41B883;
         z-index: -1;
+    }
+    .table-content {
+        width: 70vw;
+        height: 60vh;
+        box-shadow: 0px 0px 60px 40px rgb(34 60 80 / 20%);
     }
 </style>
